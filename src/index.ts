edcfbc1eab70748254e0bc1d35927320a1db45c1
Carlos Acosta <carlos.acosta@cargosprint.com>
2020-03-16 21:27:01 -0600

    first steps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import { Observable, Observer } from 'rxjs';
 
+const observer:Observer<any>={
+    next:value=>console.log('Next: ',value),
+    error: error=>console.warn('error: ',error),
+    complete: ()=>console.info('complete')
+}
 
-console.log('Hola Mundo!');
+const obs$ = new Observable(sub=>{
+        sub.next('Hola');
+        sub.next('Carlos');
 
+        sub.complete();
+        sub.next('no se ejecuta');
+        sub.next('x2');
+})
 
-
+obs$.subscribe(observer);
 
 
 
